a744e1df32f978171500d066ae8823b0730c9c09
Fachri Hawari <fachri.hawari@gmail.com>
2019-08-11 16:37:05 +0700

    test: update configration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
-  preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: "react-native",
+  transform: {
+    "^.+\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js",
+    "\\.(ts|tsx)$": "ts-jest"
+  },
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.jest.json"
+    }
+  },
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  setupFilesAfterEnv: ["./setupTests.js"]
 };
