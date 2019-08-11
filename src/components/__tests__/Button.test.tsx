8d1819eca946a03d594ef67eb2dbb6350986305a
Fachri Hawari <fachri.hawari@gmail.com>
2019-08-11 17:18:56 +0700

    test: update button test

---
+++ src/components/__tests__/Button.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
+
+import Button from '../Button';
+
+
+describe('rendering', () => {
+  
+  let wrapper: ShallowWrapper
+  const handlePress = jest.fn()
+  const buttonLabel = 'Awesome Button'
+
+  beforeEach(() => {
+    wrapper = shallow(<Button onPress={handlePress}>{buttonLabel}</Button>)
+  })
+
+  it('should render correctly', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+  
+  it('should render <Touchable/>', () => {
+    expect(wrapper.find('Touchable')).toHaveLength(1);
+  });
+
+  it('has correct button label', () => {
+    expect(wrapper.find('Text').contains(buttonLabel)).toBe(true)
+  })
+
+  it('has pressed the button', () => {
+    wrapper.simulate('press')
+    expect(handlePress).toHaveBeenCalledTimes(1)
+  })
+})
