cc1319297c85ca22df4573e5f6122e3353d611da
Fachri Hawari <fachri.hawari@gmail.com>
2019-08-13 22:42:54 +0700

    test: add auth action

---
+++ src/store/auth/__tests__/action.test.tsx
@@ -0,0 +1,93 @@
+import { parseError } from '../../helpers';
+import * as action from '../action';
+import * as constant from '../constant';
+
+describe('auth actions', () => {
+  it('should create an action to fetch user profile', () => {
+    const credentials = {
+      username: 'Username',
+      password: 'Password',
+      OTP: ''
+    }
+    const fetchUserRequestAction = action.fetchUserRequest(credentials.username, credentials.password)
+    const expectedAction = {
+      type: constant.FETCH_USER_REQUEST,
+      payload: credentials
+    }
+    expect(fetchUserRequestAction).toStrictEqual(expectedAction)
+  })
+
+  it('should create an action to fetch user profile if the user enabled 2FA', () => {
+    const credentials = {
+      username: 'Username',
+      password: 'Password',
+      OTP: '123456'
+    }
+    const fetchUserRequestAction = action.fetchUserRequest(credentials.username, credentials.password, credentials.OTP)
+    const expectedAction = {
+      type: constant.FETCH_USER_REQUEST,
+      payload: credentials
+    }
+    expect(fetchUserRequestAction).toStrictEqual(expectedAction)
+  })
+
+  it('should create an action to set loader when fetching user', () => {
+    const expectedAction = {
+      type: constant.FETCH_USER_BEGIN
+    }
+    expect(action.fetchUserBegin()).toStrictEqual(expectedAction)
+  })
+
+  it('should create an action to set profile user when success fetching user', () => {
+    const profile = {
+      id: 1,
+      name: 'User'
+    }
+    const expectedAction = {
+      type: constant.FETCH_USER_SUCCESS,
+      payload: {
+        profile
+      }
+    }
+    expect(action.fetchUserSuccess(profile)).toStrictEqual(expectedAction)
+  })
+
+
+  it('should create an action to set error message when failure fetching user because bad credentials', () => {
+    const error = {
+      response: {
+        data: {
+          message: 'Bad credentials'
+        }
+      }
+    }
+    const expectedAction = {
+      type: constant.FETCH_USER_FAILURE,
+      payload: {
+        error: parseError(error),
+        needOTP: false
+      }
+    }
+    expect(action.fetchUserFailure(parseError(error), false)).toStrictEqual(expectedAction)
+  })
+
+  it('should create an action to set error message when failure fetching user because network problem', () => {
+    const error = 'Lost connection'
+    const expectedAction = {
+      type: constant.FETCH_USER_FAILURE,
+      payload: {
+        error: parseError(error),
+        needOTP: false
+      }
+    }
+    expect(action.fetchUserFailure(parseError(error), false)).toStrictEqual(expectedAction)
+  })
+
+  it('should create an action to logout', () => {
+    const logoutAction = action.logout()
+    const expectedAction = {
+      type: constant.LOGOUT
+    }
+    expect(logoutAction).toStrictEqual(expectedAction)
+  })
+})
