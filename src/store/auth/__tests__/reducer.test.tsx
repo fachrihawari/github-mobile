8421eb82d313461a69e4e0ba227ebf24caaa5066
Fachri Hawari <fachri.hawari@gmail.com>
2019-08-14 10:01:17 +0700

    chore: run linter

--- src/store/auth/__tests__/reducer.test.tsx
+++ src/store/auth/__tests__/reducer.test.tsx
@@ -1,6 +1,6 @@
-import reducer, { initialState, IState } from '../reducer'
 import { AnyAction } from 'redux';
-import { logout, fetchUserBegin, fetchUserSuccess, fetchUserFailure } from '../action';
+import { fetchUserBegin, fetchUserFailure, fetchUserSuccess, logout } from '../action';
+import reducer, { initialState, IState } from '../reducer'
 import { transformProfile } from '../transform';
 
 describe('reducer', () => {
@@ -27,37 +27,37 @@ describe('reducer', () => {
 
   it('should return the success state', () => {
     const profile = {
-      "login": "github",
-      "id": 9919,
-      "node_id": "MDEyOk9yZ2FuaXphdGlvbjk5MTk=",
-      "avatar_url": "https://avatars1.githubusercontent.com/u/9919?v=4",
-      "gravatar_id": "",
-      "url": "https://api.github.com/users/github",
-      "html_url": "https://github.com/github",
-      "followers_url": "https://api.github.com/users/github/followers",
-      "following_url": "https://api.github.com/users/github/following{/other_user}",
-      "gists_url": "https://api.github.com/users/github/gists{/gist_id}",
-      "starred_url": "https://api.github.com/users/github/starred{/owner}{/repo}",
-      "subscriptions_url": "https://api.github.com/users/github/subscriptions",
-      "organizations_url": "https://api.github.com/users/github/orgs",
-      "repos_url": "https://api.github.com/users/github/repos",
-      "events_url": "https://api.github.com/users/github/events{/privacy}",
-      "received_events_url": "https://api.github.com/users/github/received_events",
-      "type": "Organization",
-      "site_admin": false,
-      "name": "GitHub",
-      "company": null,
-      "blog": "https://github.com/about",
-      "location": "San Francisco, CA",
-      "email": "support@github.com",
-      "hireable": null,
-      "bio": "How people build software.",
-      "public_repos": 306,
-      "public_gists": 0,
-      "followers": 0,
-      "following": 0,
-      "created_at": "2008-05-11T04:37:31Z",
-      "updated_at": "2019-07-15T14:26:15Z"
+      'login': 'github',
+      'id': 9919,
+      'node_id': 'MDEyOk9yZ2FuaXphdGlvbjk5MTk=',
+      'avatar_url': 'https://avatars1.githubusercontent.com/u/9919?v=4',
+      'gravatar_id': '',
+      'url': 'https://api.github.com/users/github',
+      'html_url': 'https://github.com/github',
+      'followers_url': 'https://api.github.com/users/github/followers',
+      'following_url': 'https://api.github.com/users/github/following{/other_user}',
+      'gists_url': 'https://api.github.com/users/github/gists{/gist_id}',
+      'starred_url': 'https://api.github.com/users/github/starred{/owner}{/repo}',
+      'subscriptions_url': 'https://api.github.com/users/github/subscriptions',
+      'organizations_url': 'https://api.github.com/users/github/orgs',
+      'repos_url': 'https://api.github.com/users/github/repos',
+      'events_url': 'https://api.github.com/users/github/events{/privacy}',
+      'received_events_url': 'https://api.github.com/users/github/received_events',
+      'type': 'Organization',
+      'site_admin': false,
+      'name': 'GitHub',
+      'company': null,
+      'blog': 'https://github.com/about',
+      'location': 'San Francisco, CA',
+      'email': 'support@github.com',
+      'hireable': null,
+      'bio': 'How people build software.',
+      'public_repos': 306,
+      'public_gists': 0,
+      'followers': 0,
+      'following': 0,
+      'created_at': '2008-05-11T04:37:31Z',
+      'updated_at': '2019-07-15T14:26:15Z'
     }
 
     currentState.isLoading = false
